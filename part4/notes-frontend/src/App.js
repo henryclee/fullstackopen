7a006ae7d88b361c42046de958d8b4a694a2a0ad
Henry Lee <henrylee@buffalo.edu>
2023-01-03 14:38:18 -0500

    Exercise 4.2

---
+++ part4/notes-frontend/src/App.js
@@ -0,0 +1,125 @@
+import { useState, useEffect } from 'react'
+//import axios from 'axios'
+import Note from './components/Note'
+
+import noteService from './services/notes'
+
+const Footer = () => {
+  const footerStyle = {
+    color: 'green',
+    fontStyle: 'italic',
+    fontSize: 16
+  }
+  return (
+    <div style = {footerStyle}>
+      <br />
+      <em>Note app, Department of Computer Science, University of Me</em>
+    </div>
+  )
+}
+
+const App = () => {
+  const [notes, setNotes] = useState([])
+  const [newNote, setNewNote] = useState('')
+  const [showAll, setShowAll] = useState(true)
+  const [errorMessage, setErrorMessage] = useState(null)
+
+  useEffect(() => {
+    noteService
+      .getAll()
+      .then(initialNotes => {
+        setNotes(initialNotes)
+      })
+  }, [])
+
+
+  const Notification = ({message}) => {
+    if (message === null) {
+      return null
+    }
+
+    return (
+      <div className = 'error'>
+        {message}
+      </div>
+    )
+  }
+
+  const toggleImportanceOf = (id) => {
+    const note = notes.find(n => n.id === id)
+    const changedNote = {...note, important: !note.important}
+
+    noteService
+      .update(id, changedNote)
+      .then (returnedNote => {
+        setNotes(notes.map(n => n.id !== id ? n : returnedNote))
+      })
+      .catch(error => {
+        console.log(error)
+        setErrorMessage(
+          `Note '${note.content}' was not present on the server`
+        )
+        setTimeout(() => {
+          setErrorMessage(null)
+        },5000)
+        setNotes(notes.filter(n => n.id !== id))
+      })
+    
+  }
+
+  const addNote = (event) => {
+    event.preventDefault()
+    const noteObject = {
+      content: newNote,
+      date: new Date().toISOString(),
+      important: Math.random() > 0.5,
+      id: notes.length + 1,
+    }
+
+    noteService
+    .create(noteObject)
+    .then(returnedNote => {
+      setNotes(notes.concat(returnedNote))
+      setNewNote('')
+    })
+  }
+
+  const handleNoteChange = (event) => {
+    setNewNote(event.target.value)
+  }
+
+  const notesToShow = showAll
+    ? notes
+    : notes.filter(note => note.important)
+
+  return (
+    <div>
+      <h1>Notes</h1>
+      <Notification message = {errorMessage} />
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? 'important' : 'all' }
+        </button>
+      </div>   
+      <ul>
+        {notesToShow.map(note => 
+          <Note 
+            key={note.id} 
+            note={note} 
+            toggleImportance = {() => toggleImportanceOf(note.id)}
+          />
+        )}
+      </ul>
+      <form onSubmit={addNote}>
+        <input
+          value={newNote}
+          onChange={handleNoteChange}
+        />
+        <button type="submit">save</button>
+      </form>
+      <Footer />
+    </div>
+  )
+}
+
+export default App
