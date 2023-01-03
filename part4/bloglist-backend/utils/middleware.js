a4995c14f7732e56d101c4b99d14d9d46cf6d62c
Henry Lee <henrylee@buffalo.edu>
2023-01-03 17:19:16 -0500

    Finished to 4.7

--- part4/bloglist-backend/utils/middleware.js
+++ part4/bloglist-backend/utils/middleware.js
@@ -1,14 +1,14 @@
 const logger = require('./logger')
 
 const unknownEndpoint = (request, response) => {
-  response.status(404).send({error: 'unknown endpoint'})
+  response.status(404).send({ error: 'unknown endpoint' })
 }
 
 const errorHandler = (error, request, response, next) => {
   logger.error(error.message)
 
-  return response.status(400).json({error: error.message})
-  
+  return response.status(400).json({ error: error.message })
+
 }
 
 module.exports = {
