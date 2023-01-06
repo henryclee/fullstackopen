b1671f648cb2598a064a21d119fa964af8d5afcc
Henry Lee <henrylee@buffalo.edu>
2023-01-05 23:38:07 -0500

    update lecture project

--- part4/notes-backend/app.js
+++ part4/notes-backend/app.js
@@ -1,11 +1,14 @@
 const config = require('./utils/config')
 const express = require('express')
+require ('express-async-errors')
 const app = express()
 const cors = require('cors')
 const notesRouter = require('./controllers/notes')
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
+
+mongoose.set('strictQuery', false)
 
 logger.info('connecting to', config.MONGODB_URI)
 
