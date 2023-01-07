419db7a798a9f5de66af4c29a4227c74b21353c0
Henry Lee <henrylee@buffalo.edu>
2023-01-07 14:43:28 -0500

    Finished lecture examples

--- part4/notes-backend/controllers/notes.js
+++ part4/notes-backend/controllers/notes.js
@@ -1,8 +1,20 @@
 const notesRouter = require('express').Router()
 const Note = require('../models/note')
+const User = require('../models/user')
+
+const jwt = require('jsonwebtoken')
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+  return null
+}
 
 notesRouter.get('/', async (request, response) => {
-  const notes = await Note.find({})
+  const notes = await Note
+    .find({}).populate('user', {username: 1, name: 1})
   response.json(notes)
 })
 
@@ -19,14 +31,27 @@ notesRouter.get('/:id', async (request, response, next) => {
 
 notesRouter.post('/', async (request, response, next) => {
   const body = request.body
+  const token = getTokenFrom(request)
+  const decodedToken = jwt.verify(token, process.env.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({error: 'token missing or invalid'})
+  }
+  const user = await User.findById(decodedToken.id)
+
+  console.log('user in post', user)
 
   const note = new Note({
     content: body.content,
-    important: body.important || false,
-    date: new Date()
+    important: body.important === undefined ? false : body.important,
+    date: new Date(),
+    user: user._id
   })
 
   const savedNote = await note.save()
+
+  user.notes = user.notes.concat(savedNote._id)
+  await user.save()
+
   response.status(201).json(savedNote)
 
 })
