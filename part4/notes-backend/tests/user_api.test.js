8864de9d7456d8beed83868e6acb187c8cd01e5e
Henry Lee <henrylee@buffalo.edu>
2023-01-08 14:12:14 -0500

    Completed part 4

--- part4/notes-backend/tests/user_api.test.js
+++ part4/notes-backend/tests/user_api.test.js
@@ -1,6 +1,6 @@
 const bcrypt = require('bcrypt')
 const User = require('../models/user')
-const mongoose = require('mongoose')
+//const mongoose = require('mongoose')
 const supertest = require('supertest')
 const helper = require('./test_helper')
 const app = require('../app')
