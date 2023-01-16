316ff58a27d01e370d8c98d9236a0dfee1d04bc3
Henry Lee <henrylee@buffalo.edu>
2023-01-16 12:25:36 -0500

    finished to Ex 6.2

---
+++ part6/notes-frontend/src/index.js
@@ -0,0 +1,9 @@
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
+
+//ReactDOM.render(document.getElementById('root')).render(<App />)
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <App/>
+)
